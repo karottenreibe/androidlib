8521e52a68878b8367dcba7edad5289a09c2a0cb
karottenreibe <git@rottenrei.be>
2011-12-24 17:40:47 +0100

    adde file utils

---
+++ AndroidLib/src/be/rottenrei/android/lib/util/FileUtils.java
@@ -0,0 +1,59 @@
+package be.rottenrei.android.lib.util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import android.content.Context;
+import android.os.Environment;
+import be.rottenrei.android.lib.R;
+
+/**
+ * Utilities for working with the Android external storage and {@link File}s in general.
+ */
+public class FileUtils {
+
+	public static boolean isExternalStorageAvailable() {
+		String state = Environment.getExternalStorageState();
+		if (Environment.MEDIA_MOUNTED.equals(state)) {
+			return true;
+		} else if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static boolean ensureExternalStorageIsAvailable(Context context) {
+		if (isExternalStorageAvailable()) {
+			return true;
+		} else {
+			UIUtils.informUser(context, R.string.no_external_storage);
+			return false;
+		}
+	}
+
+	/** Creates the directory structure above the file or warns the user that the operation failed. */
+	public static boolean ensureFileIsReadyForWriting(File file, Context context) {
+		File directory = file.getParentFile();
+		if (!directory.exists() && !directory.mkdirs()) {
+			UIUtils.informUser(context, R.string.mkdirs_failed);
+			return false;
+		}
+		return true;
+	}
+
+	public static String readWholeFile(InputStream stream) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+		StringBuilder builder = new StringBuilder();
+		String line = reader.readLine();
+		while (line != null) {
+			builder.append(line);
+			line = reader.readLine();
+		}
+		return builder.toString();
+	}
+
+}
