f5dca5edef7c0f06c8a43f99acd518968d707e1e
karottenreibe <git@rottenrei.be>
2011-12-30 11:45:32 +0100

    added a debug function that takes a long time to execute

--- AndroidLib/src/be/rottenrei/android/lib/test/util/DebugUtils.java
+++ AndroidLib/src/be/rottenrei/android/lib/test/util/DebugUtils.java
@@ -7,7 +7,7 @@ import android.util.Log;
 public class DebugUtils {
 
 	/** Logs the names of all tables in the database at warning level. */
-	public void logTableNames(SQLiteDatabase db, String tag) {
+	public static void logTableNames(SQLiteDatabase db, String tag) {
 		Cursor cursor = db.query("sqlite_master", new String[] { "name" }, "type = 'table'",
 				null, null, null, null);
 		cursor.moveToFirst();
@@ -16,6 +16,14 @@ public class DebugUtils {
 			Log.w(tag, "found table: " + name);
 			cursor.moveToNext();
 		}
+	}
+
+	public static void longOperation() {
+		long v = -1;
+		for (long i = 0; i < 100000000l; ++i) {
+			v = v + 1;
+		}
+		Log.d("long.operation", "computed: " + v);
 	}
 
 }
