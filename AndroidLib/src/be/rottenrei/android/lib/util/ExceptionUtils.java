f5f1de0b45a3d8ebc263528513eb2c18a551d1d0
karottenreibe <git@rottenrei.be>
2012-02-25 10:16:00 +0100

    shouldNeverBeReached can now take exceptions

--- AndroidLib/src/be/rottenrei/android/lib/util/ExceptionUtils.java
+++ AndroidLib/src/be/rottenrei/android/lib/util/ExceptionUtils.java
@@ -10,25 +10,28 @@ import be.rottenrei.android.lib.R;
  */
 public class ExceptionUtils {
 
-	public static void shouldNeverBeReached(Activity activity, Class<?> clazz) {
-		Exception e = new IllegalStateException("impossible option selected");
-		handleFatalExceptionWithMessage(e, activity, R.string.internal_error, clazz);
+	/** The throwable can be null. */
+	public static void shouldNeverBeReached(Activity activity, Class<?> clazz, Throwable cause) {
+		if (cause == null) {
+			cause = new IllegalStateException("An impossible state has been reached");
+		}
+		handleFatalExceptionWithMessage(cause, activity, R.string.internal_error, clazz);
 	}
 
 	/** Logs the exception and displays the warning message to the user. */
-	public static void handleExceptionWithMessage(Exception e, Context context, int message, Class<?> clazz) {
-		logException(e, context, message, clazz);
+	public static void handleExceptionWithMessage(Throwable cause, Context context, int message, Class<?> clazz) {
+		logException(cause, context, message, clazz);
 		UIUtils.informUser(context, message);
 	}
 
 	/** Just logs the exception with the given message. */
-	public static void logException(Exception e, Context context, int message, Class<?> clazz) {
-		Log.e(clazz.getName(), context.getString(message), e);
+	public static void logException(Throwable cause, Context context, int message, Class<?> clazz) {
+		Log.e(clazz.getName(), context.getString(message), cause);
 	}
 
-	/** Handles the exception and finishes the activity. */
-	public static void handleFatalExceptionWithMessage(Exception e, Context context, int message, Class<?> clazz) {
-		handleExceptionWithMessage(e, context, message, clazz);
+	/** Handles the exception and finishes the activity if possible. */
+	public static void handleFatalExceptionWithMessage(Throwable cause, Context context, int message, Class<?> clazz) {
+		handleExceptionWithMessage(cause, context, message, clazz);
 		if (context instanceof Activity) {
 			Activity activity = (Activity) context;
 			activity.finish();
